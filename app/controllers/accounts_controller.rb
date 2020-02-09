9a2d18cc71f9056f5aa0ec439045e5ba71ee5d56
Thomnas Almeida <thomas.almeida@cloudwalk.io>
2020-02-09 13:55:25 -0300

    Remove unused method

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -14,13 +14,4 @@ class AccountsController < ApplicationController
       render json: { errors: exception.message }, status: :not_found
     end
   end
-
-  # def get_balance(account)
-  #   sum_deposits, sum_withdrawals = 0, 0
-    
-  #   account.withdrawals.to_a.each { |w| sum_withdrawals += w.amount }
-  #   account.deposits.to_a.each { |w| sum_deposits += w.amount }
-
-  #   sum_deposits - sum_withdrawals
-  # end
 end
