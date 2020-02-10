07dfa8aaf276d1623f83ab9b2c9dc501b70c18a8
Thomnas Almeida <thomas.almeida@cloudwalk.io>
2020-02-10 07:33:13 -0300

    Fix create account route

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   post '/signup', to: 'users#create'
   post '/signin', to: 'auth#signin'
 
-  get '/accounts', to: 'accounts#create'
+  post '/accounts', to: 'accounts#create'
   get '/accounts/balance/:account_id', to: 'accounts#get_balance'
 
   post '/transfers', to: 'transfers#create'
